68eb5ad7cd77478dfe5b40a84b5f32f4750c2ee6
soyelyuki <n.miranda.meza@gmail.com>
2020-01-31 06:08:53 -0300

    json

---
+++ json.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+//JSON  JAva object Notation
+// aqui mismo puedo desarrollador cualquier cosa. 
+
+var  pelicula = {
+    titulo: 'batman vs Susperman',
+    year: 2017,
+    pais: 'usa',
+}
+
+console.log(pelicula)
