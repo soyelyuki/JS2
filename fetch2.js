9e84e4d2b39f4ac53d1cacae60a031122deef1e4
Nicolas Miranda Meza <nicolas@pop-os.localdomain>
2020-07-11 10:57:46 -0400

    fetch

--- fetch2.js
+++ fetch2.js
@@ -13,10 +13,12 @@ var div_janet = document.querySelector("#janet");
         })
         .then(data => data.json())
         .then(user => {
-                MostrarJanet(user);
-
+                MostrarJanet(user.data);
+                return getInfo();
+        })
+        .then(data => {
+                console.log(data);
         });
-
 
 function getUsuarios(){
         return fetch('https://reqres.in/api/users?page=2')
@@ -24,6 +26,19 @@ function getUsuarios(){
 function getJanet(){
         return fetch('https://reqres.in/api/users/2')
 }
+function getInfo(){
+        var profesor ={
+                nombre:'Nico',
+                apellidos:'miranda',
+                url:"nicolas.com"
+        };
+        return new Promise((resolve, reject) => {
+                var profesor_string = JSON.stringify(profesor);
+                if(typeof profesor_string != 'string') return reject('error');
+                return resolve(profesor_string);
+        });
+}
+
 
 function listadoUsuarios(usuarios){
         usuarios.map((user, i) => {
@@ -34,12 +49,13 @@ function listadoUsuarios(usuarios){
                 document.querySelector(".loading").style.display ='none';
         });
 }
-function MostrarJanet(usuarios){
-        usuarios.map((user, i) => {
+function MostrarJanet(user){
                 let nombre = document.createElement('h4');
-                nombre.innerHTML = i + "." + user.first_name + " " + user.last_name; 
-                div_usuarios.appendChild(nombre);
-
-                document.querySelector(".loading").style.display ='none';
-        });
+                let avatar = document.createElement('img');
+                nombre.innerHTML = 1 +"." + user.first_name + " " + user.last_name; 
+                avatar.src = user.avatar;
+                avatar.width ='100';
+                div_janet.appendChild(nombre);
+                div_janet.appendChild(avatar);
+                document.querySelector("#janet.loading").style.display ='none';
 }
