712d6f2ecec85035247a5e2a5a06dca44ea2dbd2
Nicolas Miranda Meza <nicolas@pop-os.localdomain>
2020-07-17 21:10:16 -0400

    ultimo

---
+++ fechas.js
@@ -0,0 +1,10 @@
+var fecha = new Date();
+var year = fecha.getFullYear();
+var mes =fecha.getMonth();
+var textoHora =`
+    el Año es : ${year}
+    el mes es: ${mes}
+    el dia es : ${mes}`
+
+console.log(fecha);
+console.log(Mat.random);
